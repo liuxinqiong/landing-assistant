4460ee6c1ae701388aa7873c2217983c863c0bff
liuxinqiong <372513646@qq.com>
2023-09-03 19:16:38 +0800

    feat: basic module prepare

--- config/config.default.js
+++ config/config.default.js
@@ -5,22 +5,25 @@
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
-module.exports = appInfo => {
+module.exports = (appInfo) => {
   /**
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1693736192481_5743';
 
   // add your middleware config here
-  config.middleware = [];
+  config.middleware = ['robot'];
 
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
+    robot: {
+      ua: [/curl/i, /Baiduspider/i],
+    },
   };
 
   return {
