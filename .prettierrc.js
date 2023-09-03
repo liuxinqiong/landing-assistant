075fb7792b4c2c6f661a9d756b11b68075e48cc3
liuxinqiong <372513646@qq.com>
2023-09-03 18:45:07 +0800

    fix: prettier fighting with eslint

---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+'use strict'
+// https://prettier.io/docs/en/index.html
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  proseWrap: 'never',
+  endOfLine: 'lf',
+  overrides: [
+    {
+      files: '.prettierrc',
+      options: {
+        parser: 'json',
+      },
+    },
+    {
+      files: 'document.ejs',
+      options: {
+        parser: 'html',
+      },
+    },
+  ],
+}
